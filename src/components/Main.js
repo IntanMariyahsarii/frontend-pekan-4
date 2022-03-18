fe07173f7f5638a94bdf6fdd14b81a03a0b321f0
Intan Mariyahsari <intannmariyahsari@gmail.com>
2022-03-18 19:04:17 +0700

    frontend pekan 4

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+/**
+ * Membuat component Main.
+ * Component Main menampung konten utama.
+ */
+ import Hello from "./Hello";
+
+ function Main() {
+     return (
+       <main>
+         <Hello name="Intan" />
+         <Hello name="Aufa" />
+         <Hello name="Johnson" />
+         <Hello name="John" />
+         <Hello name="Brody" />
+         <Hello name="Hans" />
+       </main>
+     );
+ }
+   
+ export default Main;
